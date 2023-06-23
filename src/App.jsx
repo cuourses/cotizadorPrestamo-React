8d6a12367482787e163ac1e4b3509af7df059108
Atoche Pascual <jmatochepascual@gmail.com>
2023-06-22 15:27:46 -1000

    [ADD] componente boton

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,18 @@ function App() {
     <div className="w-11/12 max-w-lg my-20 mx-auto shadow p-10">
       <Header />
 
+      <div className="flex justify-between my-8">
+        <Button
+          operador="-"
+          fn={handleClickDecremento}
+        />
+
+        <Button
+          operador="+"
+          fn={handleClickIncremento}
+        />
+      </div>
+
       <input
         type="range"
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
